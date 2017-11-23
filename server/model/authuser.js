2480b1fd79cb9d3b3ee1c169d9916e17ee5a9c02
Peng <pengliheng111@gmail.com>
2017-11-23 13:50:19 +0800

    class
    class static 静态写法
    model 
    方法拓展，可以给同级model一起使用
    可以在model写一些 更加复杂的查询-返回方法
    比如原本用户的所在的群只是储存群id，这个方法可以将群id替换成群的详细信息
    `authUser.find_pretty({id:userid})`
    将

--- server/model/authuser.js
+++ server/model/authuser.js
@@ -1,37 +1,46 @@
 const mongoose = require('./db.js');
-
+const {
+    Model,
+    Schema,
+} = mongoose;
 const Schema = mongoose.Schema;
-
 const authUserSchema = new Schema({
     login: String,
     name: String,
     avatar_url: String,
 });
 
-const authUserModel = mongoose.model('authuser', authUserSchema);
-
-function authUserDAO() {};
-
-authUserDAO.prototype.save = function(data) {
-    return new authUserModel(data).save();
+class authUser extends Model{
+    //因为已经extends 了 Model,所以可以直接用mongoose原生方法
+    //save 方法直接 用authUserDAO.create({id:id})
+    //update 方法直接 用authUserDAO.update({id:id},data})
+    static async findByLogin(data){
+        return this.findOne(data)
+    }
 };
 
-authUserDAO.prototype.update = function(data) {
-    let condition = {
-        login: data.login
-    };
-    return authUserModel.update(condition, data).exec();
-};
+// authUserDAO.prototype.save = function(data) {
+//     return new authUserModel(data).save();
+// };
+
+// authUserDAO.prototype.update = function(data) {
+//     let condition = {
+//         login: data.login
+//     };
+//     return authUserModel.update(condition, data).exec();
+// };
 
 
-authUserDAO.prototype.findByLogin = function(condition) {
-    return authUserModel.findOne(condition).exec();
-};
+// authUserDAO.prototype.findByLogin = function(condition) {
+//     return authUserModel.findOne(condition).exec();
+// };
 
 
-authUserDAO.prototype.find = function(condition) {
-    return authUserModel.find(condition).exec();
-};
+// authUserDAO.prototype.find = function(condition) {
+//     return authUserModel.find(condition).exec();
+// };
 
+authUserSchema.loadClass(authUser)
+const authUser = mongoose.model('users', authUserSchema);
 
-module.exports = new authUserDAO();
+module.exports = authUser;
